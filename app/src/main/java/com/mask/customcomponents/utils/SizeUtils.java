8d063df92caf2c3540d034946da1817edb1b3abf
lishilin <869370440@qq.com>
2019-12-12 17:24:38 +0800

    Raykite进度条 粒子动画

--- app/src/main/java/com/mask/customcomponents/utils/SizeUtils.java
+++ app/src/main/java/com/mask/customcomponents/utils/SizeUtils.java
@@ -47,6 +47,41 @@ public class SizeUtils {
     }
 
     /**
+     * 获取当前值百分比(根据区间获取当前值的所占百分比)
+     *
+     * @param valueCurrent 当前值
+     * @param valueStart   起始值
+     * @param valueEnd     结束值
+     * @return 百分比
+     */
+    public static float getValuePercent(float valueCurrent, float valueStart, float valueEnd) {
+        return getValuePercent(valueCurrent, valueStart, valueEnd, true);
+    }
+
+    /**
+     * 获取当前值百分比(根据区间获取当前值的所占百分比)
+     *
+     * @param valueCurrent    当前值
+     * @param valueStart      起始值
+     * @param valueEnd        结束值
+     * @param isLimitInterval 是否限制在当前区间之内
+     * @return 百分比
+     */
+    public static float getValuePercent(float valueCurrent, float valueStart, float valueEnd, boolean isLimitInterval) {
+        float percent = (valueCurrent - valueStart) / (valueEnd - valueStart);
+        if (!isLimitInterval) {
+            return percent;
+        }
+        if (percent < 0) {
+            percent = 0;
+        }
+        if (percent > 1) {
+            percent = 1;
+        }
+        return percent;
+    }
+
+    /**
      * 获取当前百分比值(根据区间获取当前所占百分比的值)
      *
      * @param percent    当前百分比
--- app/src/main/java/com/mask/customcomponents/view/RaykiteProgressBar.java
+++ app/src/main/java/com/mask/customcomponents/view/RaykiteProgressBar.java
@@ -17,11 +17,114 @@ import android.view.View;
 import com.mask.customcomponents.utils.NumberUtils;
 import com.mask.customcomponents.utils.SizeUtils;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Raykite进度条
  * Created by lishilin on 2019/12/11
  */
 public class RaykiteProgressBar extends View {
+
+    /**
+     * 粒子实体类
+     */
+    public static class Particle {
+
+        private long createTimeX;// X轴动画创建时间
+        private long createTimeY;// Y轴动画创建时间
+        private long durationX;// X轴动画时长
+        private long durationY;// Y轴动画时长
+
+        public float width;// 宽
+
+        public Particle(long durationX, long durationY, float width) {
+            this.createTimeX = System.currentTimeMillis();
+            this.createTimeY = System.currentTimeMillis();
+            this.durationX = durationX;
+            this.durationY = durationY;
+            this.width = width;
+        }
+
+        /**
+         * 重置
+         *
+         * @param durationY durationY
+         * @param width     width
+         */
+        public void reset(long durationY, float width) {
+            this.createTimeX = System.currentTimeMillis();
+            this.durationY = durationY;
+            this.width = width;
+        }
+
+        /**
+         * 是否可以运行
+         *
+         * @return boolean
+         */
+        public boolean isEnable() {
+            return System.currentTimeMillis() - createTimeX <= durationX;
+        }
+
+        /**
+         * 获取 当前Alpha
+         *
+         * @param alphaStart alphaStart
+         * @param alphaEnd   alphaEnd
+         * @return int
+         */
+        public int getCurrentAlpha(int alphaStart, int alphaEnd) {
+            return (int) (SizeUtils.getPercentValue(getCurrentPercentX(), alphaStart, alphaEnd));
+        }
+
+        /**
+         * 获取 当前PointX
+         *
+         * @param pointXStart pointXStart
+         * @param pointXEnd   pointXEnd
+         * @return float
+         */
+        public float getCurrentPointX(float pointXStart, float pointXEnd) {
+            return SizeUtils.getPercentValue(getCurrentPercentX(), pointXStart, pointXEnd);
+        }
+
+        /**
+         * 获取 当前PointY
+         *
+         * @param pointYStart pointYStart
+         * @param pointYEnd   pointYEnd
+         * @return float
+         */
+        public float getCurrentPointY(float pointYStart, float pointYEnd) {
+            return SizeUtils.getPercentValue(getCurrentPercentY(), pointYStart, pointYEnd);
+        }
+
+        /**
+         * 获取 X轴当前百分比
+         *
+         * @return float
+         */
+        private float getCurrentPercentX() {
+            return ((System.currentTimeMillis() - createTimeX) % durationX) * 1.0f / durationX;
+        }
+
+        /**
+         * 获取 Y轴当前百分比
+         *
+         * @return float
+         */
+        private float getCurrentPercentY() {
+            long time = (System.currentTimeMillis() - createTimeY + durationY / 2) % (durationY * 2);
+            if (time <= durationY) {
+                return SizeUtils.getValuePercent(time, 0, durationY);
+            } else {
+                return SizeUtils.getValuePercent(time - durationY, durationY, 0);
+            }
+        }
+
+    }
 
     private final int backgroundColor = 0xFF21232C;// 背景颜色
     private final Shader barShader = new LinearGradient(0, 0, 1, 0, new int[]{0xFF3B5BDC, 0xFF6F72ED}, null, Shader.TileMode.CLAMP);// 进度条颜色
@@ -29,11 +132,21 @@ public class RaykiteProgressBar extends View {
     private final int textColor = Color.WHITE;// 文本颜色
 
     private final int radius = SizeUtils.dpToPx(2);// 圆角半径
-    private final int[] particleWidthArr = new int[]{SizeUtils.dpToPx(6), SizeUtils.dpToPx(3), SizeUtils.dpToPx(4), SizeUtils.dpToPx(10)};// 粒子宽度集合
+    private final int[] particleWidthArr = new int[]{8, 5, 5, 6, 9, 6, 4, 3};// 粒子宽度集合
+    private final int[] particleDurationYArr = new int[]{800, 500, 500, 600, 900, 600, 400, 300};// 粒子Y轴动画周期集合
     private final int fontText = SizeUtils.dpToPx(13);// 文本文字大小
+
+    private final long particleDurationX = 2000;// 粒子X轴动画周期
+    private final long particleSpeed = 150;// 粒子的创建速度
+    private final List<Particle> particleList = new ArrayList<>();// 粒子集合
+    private final List<Particle> particlePool = new ArrayList<>();// 粒子缓存池
+    private int particleNextCreateIndex;// 下一次创建粒子的下标
+    private long particleLastCreateTime;// 最后一次创建粒子的时间
 
     private int width;// 宽
     private int height;// 高
+
+    private float particleContentWidth;// 粒子整体宽
 
     private Paint backgroundPaint;// Paint 背景
     private Paint particlePaint;// Paint 粒子
@@ -114,7 +227,7 @@ public class RaykiteProgressBar extends View {
      * 刷新 数据
      */
     protected void refreshData() {
-
+        particleContentWidth = width / 7.0f;
     }
 
     @Override
@@ -131,6 +244,7 @@ public class RaykiteProgressBar extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBackground(canvas);
+        drawParticle(canvas);
         drawBar(canvas);
         drawText(canvas);
     }
@@ -172,6 +286,79 @@ public class RaykiteProgressBar extends View {
         canvas.drawText(percentStr, textX, textY, textPaint);
     }
 
+    /* ********************************************* 粒子相关 **********************************************/
+
+    /**
+     * 绘制 粒子
+     *
+     * @param canvas canvas
+     */
+    private void drawParticle(Canvas canvas) {
+        float left = SizeUtils.getPercentValue(percent, 0, width);
+        float right = left + particleContentWidth;
+        float top = 0;
+        float bottom = top + height;
+        if (left >= width) {
+            return;
+        }
+
+        createParticle();
+
+        // 裁切
+        canvas.save();
+        canvas.clipRect(0, top, right, bottom);
+
+        Iterator<Particle> iterator = particleList.iterator();
+        while (iterator.hasNext()) {
+            Particle particle = iterator.next();
+            if (particle.isEnable()) {
+                particlePaint.setAlpha(particle.getCurrentAlpha(0xFF, 0x00));
+
+                float pointX = particle.getCurrentPointX(left, right);
+                float pointY = particle.getCurrentPointY(top, bottom);
+                float widthHalf = particle.width / 2;
+                canvas.drawRoundRect(pointX - widthHalf, pointY - widthHalf, pointX + widthHalf, pointY + widthHalf, radius, radius, particlePaint);
+            } else {
+                iterator.remove();
+                particlePool.add(particle);
+            }
+        }
+
+        canvas.restore();
+
+        postInvalidateOnAnimation();
+    }
+
+    /**
+     * 创建 粒子
+     */
+    private void createParticle() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - particleLastCreateTime < particleSpeed) {
+            return;
+        }
+
+        if (particleNextCreateIndex >= particleWidthArr.length) {
+            particleNextCreateIndex = 0;
+        }
+        int particleWidth = SizeUtils.dpToPx(particleWidthArr[particleNextCreateIndex]);
+        int particleDurationY = particleDurationYArr[particleNextCreateIndex];
+
+        Particle particle;
+        if (particlePool.size() > 0) {
+            particle = particlePool.get(0);
+            particlePool.remove(0);
+            particle.reset(particleDurationY, particleWidth);
+        } else {
+            particle = new Particle(particleDurationX, particleDurationY, particleWidth);
+        }
+        particleList.add(particle);
+
+        particleNextCreateIndex++;
+        particleLastCreateTime = currentTime;
+    }
+    /* ********************************************* 粒子相关 **********************************************/
+
     /* ********************************************* 内部方法 **********************************************/
 
     /**
@@ -183,7 +370,6 @@ public class RaykiteProgressBar extends View {
     public String getValuePercentStr(float percent) {
         return NumberUtils.multiply(NumberUtils.getRoundDecimal(percent, 3), 100) + "%";
     }
-
     /* ********************************************* 内部方法 **********************************************/
 
     /* ********************************************* 外部调用 **********************************************/
