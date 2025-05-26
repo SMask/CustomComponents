38014f6247b19fc112f8365aba78350919cc13a2
Mask <869370440@qq.com>
2025-05-26 11:15:15 +0800

    可拖拽控件 添加 拖拽、吸附、动画 功能

--- app/src/main/java/com/mask/customcomponents/activity/DraggableViewActivity.kt
+++ app/src/main/java/com/mask/customcomponents/activity/DraggableViewActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mask.customcomponents.databinding.ActivityDraggableViewBinding
+import com.mask.customcomponents.utils.LogUtil
+import com.mask.customcomponents.utils.ToastUtils
+import com.mask.customcomponents.view.DragGestureDetector
 
 /**
  * 可拖拽控件
@@ -13,6 +16,14 @@ class DraggableViewActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityDraggableViewBinding.inflate(layoutInflater)
+    }
+
+    private val dragGestureDetectorConstraint by lazy {
+        DragGestureDetector(binding.viewConstraint)
+    }
+
+    private val dragGestureDetectorFrame by lazy {
+        DragGestureDetector(binding.viewFrame)
     }
 
     companion object {
@@ -34,8 +45,39 @@ class DraggableViewActivity : AppCompatActivity() {
     }
 
     private fun setListener() {
+        binding.viewConstraint.setOnClickListener {
+            LogUtil.i("onClick ConstraintLayout")
+            ToastUtils.show(this, "View ConstraintLayout")
+        }
+        binding.viewFrame.setOnClickListener {
+            LogUtil.i("onClick FrameLayout")
+            ToastUtils.show(this, "View FrameLayout")
+        }
+        binding.cbOutOfBounds.setOnCheckedChangeListener { _, isChecked ->
+            dragGestureDetectorConstraint.setOutOfBoundsEnabled(isChecked)
+            dragGestureDetectorFrame.setOutOfBoundsEnabled(isChecked)
+        }
+        binding.cbAnim.setOnCheckedChangeListener { _, isChecked ->
+            dragGestureDetectorConstraint.setAnimEnabled(isChecked)
+            dragGestureDetectorFrame.setAnimEnabled(isChecked)
+        }
+        binding.cbSnapHorizontal.setOnCheckedChangeListener { _, isChecked ->
+            dragGestureDetectorConstraint.setHorizontalSnapEnabled(isChecked)
+            dragGestureDetectorFrame.setHorizontalSnapEnabled(isChecked)
+        }
+        binding.cbSnapVertical.setOnCheckedChangeListener { _, isChecked ->
+            dragGestureDetectorConstraint.setVerticalSnapEnabled(isChecked)
+            dragGestureDetectorFrame.setVerticalSnapEnabled(isChecked)
+        }
+
+        dragGestureDetectorConstraint.attach()
+        dragGestureDetectorFrame.attach()
     }
 
     private fun initData() {
+        binding.cbOutOfBounds.setChecked(false)
+        binding.cbAnim.setChecked(true)
+        binding.cbSnapHorizontal.setChecked(true)
+        binding.cbSnapVertical.setChecked(true)
     }
 }
---
+++ app/src/main/java/com/mask/customcomponents/view/DragGestureDetector.kt
@@ -0,0 +1,418 @@
+package com.mask.customcomponents.view
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+
+/**
+ * 拖拽 GestureDetector
+ *
+ * Create by lishilin on 2025-05-23
+ */
+class DragGestureDetector(val view: View) {
+
+    /**
+     * 位置
+     */
+    private enum class Location {
+        LEFT_TOP, LEFT_BOTTOM, RIGHT_TOP, RIGHT_BOTTOM
+    }
+
+    // 初始数据
+    private var leftMargin = 0
+    private var topMargin = 0
+    private var rightMargin = 0
+    private var bottomMargin = 0
+
+    // 手势操作中数据
+    private var lastRawX = 0f
+    private var lastRawY = 0f
+    private var isDragging = false // 是否正在拖拽
+
+    // 内部配置项
+    private val touchSlop by lazy {
+        ViewConfiguration.get(view.context).scaledTouchSlop
+    } // 拖拽阈值（系统默认触摸敏感度）
+    private val animDuration = 150L // 动画时长
+
+    // 外部可修改配置项
+    private var isOutOfBoundsEnabled = false // 是否允许超出边界
+    private var isHorizontalSnapEnabled = true // 是否开启水平吸附
+    private var isVerticalSnapEnabled = false // 是否开启垂直吸附
+    private var isAnimEnabled = true // 是否开启动画
+
+    private fun init() {
+        val layoutParams = view.layoutParams as? ViewGroup.MarginLayoutParams
+        if (layoutParams != null) {
+            leftMargin = layoutParams.leftMargin
+            topMargin = layoutParams.topMargin
+            rightMargin = layoutParams.rightMargin
+            bottomMargin = layoutParams.bottomMargin
+        }
+    }
+
+    private fun onTouchEvent(event: MotionEvent): Boolean {
+        val rawX = event.rawX
+        val rawY = event.rawY
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                lastRawX = rawX
+                lastRawY = rawY
+                isDragging = false
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                val dx = rawX - lastRawX
+                val dy = rawY - lastRawY
+
+                if (!isDragging && ((dx * dx + dy * dy) > touchSlop * touchSlop)) {
+                    isDragging = true
+                }
+
+                if (isDragging) {
+                    onDragging(dx, dy)
+                    lastRawX = rawX
+                    lastRawY = rawY
+                }
+            }
+
+            MotionEvent.ACTION_UP -> {
+                if (isDragging) {
+                    onDragEnd()
+                    isDragging = false
+                    return true
+                }
+            }
+
+            MotionEvent.ACTION_CANCEL -> {
+                if (isDragging) {
+                    onDragEnd()
+                    isDragging = false
+                }
+            }
+        }
+        return isDragging
+    }
+
+    /**
+     * 拖拽中
+     */
+    private fun onDragging(dx: Float, dy: Float) {
+        val x = view.x + dx
+        val y = view.y + dy
+        if (isOutOfBoundsEnabled) {
+            view.x = x
+            view.y = y
+        } else {
+            view.x = x.coerceAtLeast(getBoundHorizontalMin()).coerceAtMost(getBoundHorizontalMax())
+            view.y = y.coerceAtLeast(getBoundVerticalMin()).coerceAtMost(getBoundVerticalMax())
+        }
+    }
+
+    /**
+     * 拖拽结束
+     */
+    private fun onDragEnd() {
+        // 获取 View 最终的位置，吸附等逻辑
+        val location = getLocation()
+        val x = if (isHorizontalSnapEnabled) {
+            if (location == Location.LEFT_TOP || location == Location.LEFT_BOTTOM) {
+                getBoundHorizontalMin()
+            } else {
+                getBoundHorizontalMax()
+            }
+        } else {
+            view.x
+        }
+        val y = if (isVerticalSnapEnabled) {
+            if (location == Location.LEFT_TOP || location == Location.RIGHT_TOP) {
+                getBoundVerticalMin()
+            } else {
+                getBoundVerticalMax()
+            }
+        } else {
+            view.y
+        }
+
+        // 判断是否需要移动 View
+        if (x == view.x && y == view.y) {
+            // 不需要移动 View，直接更新 LayoutParams
+            updateLayoutParams()
+            return
+        }
+        if (isAnimEnabled) {
+            // 执行移动动画，完毕后更新 LayoutParams
+            view.animate()
+                .setInterpolator(DecelerateInterpolator())
+                .setDuration(animDuration)
+                .x(x)
+                .y(y)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        super.onAnimationEnd(animation)
+                        updateLayoutParams()
+                    }
+                })
+                .start()
+        } else {
+            // 直接移动 View，然后更新 LayoutParams
+            view.x = x
+            view.y = y
+            updateLayoutParams()
+        }
+    }
+
+    /**
+     * 获取 边界值
+     */
+    private fun getBoundHorizontalMin(): Float {
+        val parentView = view.parent as? ViewGroup ?: return 0f
+        return (parentView.paddingLeft + leftMargin).toFloat()
+    }
+
+    /**
+     * 获取 边界值
+     */
+    private fun getBoundHorizontalMax(): Float {
+        val parentView = view.parent as? ViewGroup ?: return 0f
+        return (parentView.width - parentView.paddingRight - rightMargin - view.width).toFloat()
+    }
+
+    /**
+     * 获取 边界值
+     */
+    private fun getBoundVerticalMin(): Float {
+        val parentView = view.parent as? ViewGroup ?: return 0f
+        return (parentView.paddingTop + topMargin).toFloat()
+    }
+
+    /**
+     * 获取 边界值
+     */
+    private fun getBoundVerticalMax(): Float {
+        val parentView = view.parent as? ViewGroup ?: return 0f
+        return (parentView.height - parentView.paddingBottom - bottomMargin - view.height).toFloat()
+    }
+
+    /**
+     * 获取 位置
+     */
+    private fun getLocation(): Location {
+        val parentView = view.parent as? ViewGroup ?: return Location.LEFT_TOP
+        val parentCenterX = parentView.width / 2f
+        val parentCenterY = parentView.height / 2f
+        val viewCenterX = view.x + view.width / 2f
+        val viewCenterY = view.y + view.height / 2f
+
+        return if (viewCenterX <= parentCenterX) {
+            if (viewCenterY <= parentCenterY) {
+                Location.LEFT_TOP
+            } else {
+                Location.LEFT_BOTTOM
+            }
+        } else {
+            if (viewCenterY <= parentCenterY) {
+                Location.RIGHT_TOP
+            } else {
+                Location.RIGHT_BOTTOM
+            }
+        }
+    }
+
+    /**
+     * 更新 LayoutParams，保存最终位置，确保位置在屏幕旋转或重新布局时保持不变。
+     */
+    private fun updateLayoutParams() {
+        val layoutParams = view.layoutParams
+
+        // 设置位置
+        val isUpdateLayoutParams = when (layoutParams) {
+            is FrameLayout.LayoutParams -> {
+                updateLayoutParamsGravity(layoutParams)
+                true
+            }
+
+            is ConstraintLayout.LayoutParams -> {
+                updateLayoutParamsGravity(layoutParams)
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+
+        // 没更新 LayoutParams，直接返回
+        if (!isUpdateLayoutParams) {
+            return
+        }
+
+        // 重置偏移量
+        view.translationX = 0f
+        view.translationY = 0f
+
+        // 重新设置 LayoutParams，不然 ConstraintLayout 布局不会生效
+        view.layoutParams = layoutParams
+    }
+
+    private fun updateLayoutParamsGravity(layoutParams: FrameLayout.LayoutParams) {
+        // 重新设置位置
+        when (getLocation()) {
+            Location.LEFT_TOP -> {
+                layoutParams.gravity = Gravity.LEFT or Gravity.TOP
+            }
+
+            Location.LEFT_BOTTOM -> {
+                layoutParams.gravity = Gravity.LEFT or Gravity.BOTTOM
+            }
+
+            Location.RIGHT_TOP -> {
+                layoutParams.gravity = Gravity.RIGHT or Gravity.TOP
+            }
+
+            Location.RIGHT_BOTTOM -> {
+                layoutParams.gravity = Gravity.RIGHT or Gravity.BOTTOM
+            }
+        }
+
+        // 设置外边距
+        updateLayoutParamsMargin(layoutParams)
+    }
+
+    private fun updateLayoutParamsGravity(layoutParams: ConstraintLayout.LayoutParams) {
+        // 重置布局参数，避免其他约束关系影响控件位置
+        layoutParams.guideBegin = ConstraintSet.UNSET
+        layoutParams.guideEnd = ConstraintSet.UNSET
+        layoutParams.guidePercent = ConstraintSet.UNSET.toFloat()
+        layoutParams.leftToLeft = ConstraintSet.UNSET
+        layoutParams.leftToRight = ConstraintSet.UNSET
+        layoutParams.rightToLeft = ConstraintSet.UNSET
+        layoutParams.rightToRight = ConstraintSet.UNSET
+        layoutParams.topToTop = ConstraintSet.UNSET
+        layoutParams.topToBottom = ConstraintSet.UNSET
+        layoutParams.bottomToTop = ConstraintSet.UNSET
+        layoutParams.bottomToBottom = ConstraintSet.UNSET
+        layoutParams.baselineToBaseline = ConstraintSet.UNSET
+        layoutParams.baselineToTop = ConstraintSet.UNSET
+        layoutParams.baselineToBottom = ConstraintSet.UNSET
+        layoutParams.circleConstraint = ConstraintSet.UNSET
+        layoutParams.startToEnd = ConstraintSet.UNSET
+        layoutParams.startToStart = ConstraintSet.UNSET
+        layoutParams.endToStart = ConstraintSet.UNSET
+        layoutParams.endToEnd = ConstraintSet.UNSET
+        layoutParams.horizontalWeight = ConstraintSet.UNSET.toFloat()
+        layoutParams.verticalWeight = ConstraintSet.UNSET.toFloat()
+        layoutParams.editorAbsoluteX = ConstraintSet.UNSET
+        layoutParams.editorAbsoluteY = ConstraintSet.UNSET
+        layoutParams.orientation = ConstraintSet.UNSET
+
+        // 重新设置约束关系
+        when (getLocation()) {
+            Location.LEFT_TOP -> {
+                layoutParams.leftToLeft = ConstraintSet.PARENT_ID
+                layoutParams.topToTop = ConstraintSet.PARENT_ID
+            }
+
+            Location.LEFT_BOTTOM -> {
+                layoutParams.leftToLeft = ConstraintSet.PARENT_ID
+                layoutParams.bottomToBottom = ConstraintSet.PARENT_ID
+            }
+
+            Location.RIGHT_TOP -> {
+                layoutParams.rightToRight = ConstraintSet.PARENT_ID
+                layoutParams.topToTop = ConstraintSet.PARENT_ID
+            }
+
+            Location.RIGHT_BOTTOM -> {
+                layoutParams.rightToRight = ConstraintSet.PARENT_ID
+                layoutParams.bottomToBottom = ConstraintSet.PARENT_ID
+            }
+        }
+
+        // 设置外边距
+        updateLayoutParamsMargin(layoutParams)
+    }
+
+    private fun updateLayoutParamsMargin(layoutParams: ViewGroup.MarginLayoutParams) {
+        val left = view.x
+        val top = view.y
+        val right = left + view.width
+        val bottom = top + view.height
+
+        val parentView = view.parent as? ViewGroup
+        val parentWidth = parentView?.width ?: 0
+        val parentHeight = parentView?.height ?: 0
+        val parentPaddingLeft = parentView?.paddingLeft ?: 0
+        val parentPaddingTop = parentView?.paddingTop ?: 0
+        val parentPaddingRight = parentView?.paddingRight ?: 0
+        val parentPaddingBottom = parentView?.paddingBottom ?: 0
+
+        val leftMargin = (left - parentPaddingLeft).toInt()
+        val topMargin = (top - parentPaddingTop).toInt()
+        val rightMargin = (parentWidth - right - parentPaddingRight).toInt()
+        val bottomMargin = (parentHeight - bottom - parentPaddingBottom).toInt()
+
+        when (getLocation()) {
+            Location.LEFT_TOP -> {
+                layoutParams.leftMargin = leftMargin
+                layoutParams.topMargin = topMargin
+            }
+
+            Location.LEFT_BOTTOM -> {
+                layoutParams.leftMargin = leftMargin
+                layoutParams.bottomMargin = bottomMargin
+            }
+
+            Location.RIGHT_TOP -> {
+                layoutParams.rightMargin = rightMargin
+                layoutParams.topMargin = topMargin
+            }
+
+            Location.RIGHT_BOTTOM -> {
+                layoutParams.rightMargin = rightMargin
+                layoutParams.bottomMargin = bottomMargin
+            }
+        }
+    }
+
+    /************************************************************ S 外部调用 ************************************************************/
+
+    @SuppressLint("ClickableViewAccessibility")
+    fun attach() {
+        init()
+        view.setOnTouchListener { _, event ->
+            onTouchEvent(event)
+        }
+    }
+
+    fun setOutOfBoundsEnabled(isOutOfBoundsEnabled: Boolean): DragGestureDetector {
+        this.isOutOfBoundsEnabled = isOutOfBoundsEnabled
+        return this
+    }
+
+    fun setHorizontalSnapEnabled(isHorizontalSnapEnabled: Boolean): DragGestureDetector {
+        this.isHorizontalSnapEnabled = isHorizontalSnapEnabled
+        return this
+    }
+
+    fun setVerticalSnapEnabled(isVerticalSnapEnabled: Boolean): DragGestureDetector {
+        this.isVerticalSnapEnabled = isVerticalSnapEnabled
+        return this
+    }
+
+    fun setAnimEnabled(isAnimEnabled: Boolean): DragGestureDetector {
+        this.isAnimEnabled = isAnimEnabled
+        return this
+    }
+
+    /************************************************************ E 外部调用 ************************************************************/
+}
